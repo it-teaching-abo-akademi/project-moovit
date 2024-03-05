7aa76dff6f13de19afd8fd99eca650d4804cbbcb
Fredrik Wasström <fredrik.wasstrom@gmail.com>
2024-03-05 19:08:31 +0200

    add http

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-const baseURL = "ec2-34-236-145-67.compute-1.amazonaws.com"
+const baseURL = "http://ec2-34-236-145-67.compute-1.amazonaws.com"
 const port = "8080"
 
 export const USERSSERVICE =      baseURL+":"+port+"/api/users";
